83fad1141101cf011b9902c20d94658d2f247bb7
Francisco Manuel Cámara Saura <fmcs@alu.ua.es>
2017-02-27 18:54:52 +0100

    Migración de LinPedido para crear su DB

---
+++ pcbuild/database/migrations/2017_02_27_103446_create_linpedido_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinpedidoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('linpedido', function (Blueprint $table) {
+            $table->integer('num');
+            $table->integer('cantidad');
+            $table->integer('producto');
+            $table->integer('pedido');
+            $table->primary(['num', 'producto']);
+            $table->foreign('producto')->references('id')->on('productos');
+            $table->foreign('pedido')->references('id')->on('pedidos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('linpedido');
+    }
+}
