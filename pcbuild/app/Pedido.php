837eb752f706fc75be7a604669f2d7d13fad8cf6
ALEJANDRO OLIVER RUIZ <aor14@clL01-15>
2017-02-27 11:51:59 +0100

    Relaciones implementadas en el modelo Pedido

--- pcbuild/app/Pedido.php
+++ pcbuild/app/Pedido.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    public function usuario(){
+		return $this->belongsTo('App\Usuario');
+	}
+    public function linPedidos()
+    {
+     return $this->hasMany('App\LinPedido');
+    }
 }
