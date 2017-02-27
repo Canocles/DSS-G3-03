81dc318788b5b65cda5c52850a0a0a7d61fb82ff
Francisco Manuel Cámara Saura <fmcs@alu.ua.es>
2017-02-27 18:52:02 +0100

    Relaciones implementadas en el modelo de LinPedido

---
+++ pcbuild/app/LinPedido.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LinPedido {
+    private $num;
+    private $cantidad;
+    private $producto;
+
+    private function __constructor ($num, $cantidad, $producto) {
+        setNun ($num);
+        setCantidad ($cantidad);
+        setProducto ($producto);
+    }
+
+    public function getNum () {
+        return $this->num;
+    }
+
+    public function getCantidad () {
+        return $this->cantidad;
+    }
+
+    public function getProducto () {
+        return $this->producto;
+    }
+
+    public function setNun ($num) {
+        $this->num=$num;
+    }
+
+    public function setCantidad ($cantidad) {
+        $this->cantidad=$cantidad;
+    }
+
+    public function setProducto ($producto) {
+        $this->producto=$producto;
+    }
+
+    public function cambiarCantidad ($num, $cantidad) {
+        setCantidad ($cantidad);
+    }
+}
+
+?>
