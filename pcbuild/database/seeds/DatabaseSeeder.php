90a9da5bc82c4e3a6cfa4f5aec33342a9dae0a39
Pablo Mateo <pmp64@alu.ua.es>
2017-03-01 09:43:33 +0100

    Cambiado el orden de seeders (usuarios/pedidos)

--- pcbuild/database/seeds/DatabaseSeeder.php
+++ pcbuild/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(ProductosTableSeeder::class);
-        $this->call(PedidosTableSeeder::class);
         $this->call(UsuariosTableSeeder::class);
+	$this->call(PedidosTableSeeder::class);
     }
 }
