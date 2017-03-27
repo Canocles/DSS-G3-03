f16f7abc330bf0a601796ce946682693186a81fd
PABLO MATEO PEREZ <pmp64@clL01-15>
2017-03-27 12:43:37 +0200

    Añadidos métodos CRUD básicos al controlador de productos

--- pcbuild/app/Http/Controllers/ProductosController.php
+++ pcbuild/app/Http/Controllers/ProductosController.php
@@ -6,11 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductosController extends Controller
 {
-
+	//Consultas y búsquedas
     public function dameTodos(){
         $productos = DB::table('productos')->get();
         return $productos;
     }
+    
+    public function buscaID($id){
+		$producto = Producto::findOrFail($id);
+		return $producto;
+	}
 
     public function buscarNombre($nombre){
         $productos = DB::table('productos')->where('nombre', $nombre)->get();
@@ -32,4 +37,35 @@ class ProductosController extends Controller
         ])->get();
         return $productos;
     }
+    
+    //Create
+    public function anadirProducto($nombre, $precio, $urlImagen, $descripcion, $categoria){
+		$producto = new Producto();
+		$producto->nombre = $nombre;
+		$producto->precio = $precio;
+		$producto->urlImagen = $urlImagen;
+		$producto->descripcion = $descripcion;
+		$producto->categoria = $categoria;
+		
+		$producto->save();
+	}
+	
+	//Update
+	public function update($id, $nombre, $precio, $urlImagen, $descripcion, $categoria){
+		$producto = Producto::findOrFail($id);
+		
+		$producto->nombre = $nombre;
+		$producto->precio = $precio;
+		$producto->urlImagen = $urlImagen;
+		$producto->descripcion = $descripcion;
+		$producto->categoria = $categoria;
+		
+		$producto->save();
+	}
+	
+	//Delete
+	public function borrar($id){
+		$product = Product::find($id);
+		$product->delete();
+	}
 }
