b5dd87c9b44fcc8a2d2940a21e3ec8daf552f8a2
pmp64 <pmp64@alu.ua.es>
2017-05-15 11:53:11 +0200

    Creado middleware de autentificación de Administrador/Protegidas rutas de administrador

--- pcbuild/routes/web.php
+++ pcbuild/routes/web.php
@@ -19,54 +19,54 @@ Route::get('buscar','ProductosController@buscarNombre');
 Route::get('ordenar','ProductosController@buscarPrecio');
 
 // Listados de todos los objetos
-Route::get('admin', 'ProductosController@dameAdmin');
-Route::get('admin/productos', 'ProductosController@mostrarProductos');
-Route::get('admin/usuarios', 'UsuariosController@mostrarUsuarios');
-Route::get('admin/categorias', 'CategoriasController@mostrarCategorias');
-Route::get('admin/pedidos', 'PedidosController@mostrarPedidos');
-Route::get('admin/linpedidos', 'LinPedidosController@mostrarLinpedidos');
-Route::get('admin/pedidos/pedido/{id}', ['as' => 'admin/pedidos/pedido', 'uses' => 'PedidosController@mostrarPorPedido']);
+Route::get('admin', 'ProductosController@dameAdmin')->middleware('admin');
+Route::get('admin/productos', 'ProductosController@mostrarProductos')->middleware('admin');
+Route::get('admin/usuarios', 'UsuariosController@mostrarUsuarios')->middleware('admin');
+Route::get('admin/categorias', 'CategoriasController@mostrarCategorias')->middleware('admin');
+Route::get('admin/pedidos', 'PedidosController@mostrarPedidos')->middleware('admin');
+Route::get('admin/linpedidos', 'LinPedidosController@mostrarLinpedidos')->middleware('admin');
+Route::get('admin/pedidos/pedido/{id}', ['as' => 'admin/pedidos/pedido', 'uses' => 'PedidosController@mostrarPorPedido'])->middleware('admin');
 
 // Ordenado de un campo de los listados
-Route::get('admin/productos/ordenar/precio/{oredn}', 'ProductosController@ordenar');
-Route::get('admin/usuarios/ordenar/nombre/{oredn}', 'UsuariosController@ordenar');
-Route::get('admin/categorias/ordenar/nombre/{oredn}', 'CategoriasController@ordenar');
-Route::get('admin/pedidos/ordenar/pedido/{oredn}', 'PedidosController@ordenar');
-Route::get('admin/linpedidos/ordenar/pedido/{oredn}', 'LinPedidosController@ordenar');
+Route::get('admin/productos/ordenar/precio/{oredn}', 'ProductosController@ordenar')->middleware('admin');
+Route::get('admin/usuarios/ordenar/nombre/{oredn}', 'UsuariosController@ordenar')->middleware('admin');
+Route::get('admin/categorias/ordenar/nombre/{oredn}', 'CategoriasController@ordenar')->middleware('admin');
+Route::get('admin/pedidos/ordenar/pedido/{oredn}', 'PedidosController@ordenar')->middleware('admin');
+Route::get('admin/linpedidos/ordenar/pedido/{oredn}', 'LinPedidosController@ordenar')->middleware('admin');
 
 // Rutas para mostrar los formularios para añadir objetos
-Route::get('admin/productos/anadir', 'ProductosController@dameCategorias');
-Route::get('admin/usuarios/anadir', function() { return view ('altausuario'); });
-Route::get('admin/categorias/anadir', function() { return view ('altacategoria'); });
-Route::get('admin/pedidos/anadir', function() { return view ('altapedido'); });
-Route::get('admin/linpedidos/anadir', function() { return view('altalinpedido'); });
+Route::get('admin/productos/anadir', 'ProductosController@dameCategorias')->middleware('admin');
+Route::get('admin/usuarios/anadir', function() { return view ('altausuario'); })->middleware('admin');
+Route::get('admin/categorias/anadir', function() { return view ('altacategoria'); })->middleware('admin');
+Route::get('admin/pedidos/anadir', function() { return view ('altapedido'); })->middleware('admin');
+Route::get('admin/linpedidos/anadir', function() { return view('altalinpedido'); })->middleware('admin');
 Route::get('contacto',function(){ return view('contacto'); });
 // Rutas para mostrar los formularios para modificar objetos
-Route::get('admin/productos/modificar/{id}', ['as' => 'admin/productos/modificar', 'uses' => 'ProductosController@mostrarProducto']);
-Route::get('admin/usuarios/modificar/{id}', ['as' => 'admin/usuarios/modificar', 'uses' => 'UsuariosController@mostrarUsuario']);
-Route::get('admin/categorias/modificar/{id}', ['as' => 'admin/categorias/modificar', 'uses' => 'CategoriasController@mostrarCategoria']);
-Route::get('admin/pedidos/modificar/{id}', ['as' => 'admin/pedidos/modificar', 'uses' => 'PedidosController@mostrarPedido']);
-Route::get('admin/linpedidos/modificar/{num}/{pedido_id}', ['as' => 'admin/linpedidos/modificar', 'uses' => 'LinPedidosController@mostrarLinpedidosPedido']);
+Route::get('admin/productos/modificar/{id}', ['as' => 'admin/productos/modificar', 'uses' => 'ProductosController@mostrarProducto'])->middleware('admin');
+Route::get('admin/usuarios/modificar/{id}', ['as' => 'admin/usuarios/modificar', 'uses' => 'UsuariosController@mostrarUsuario'])->middleware('admin');
+Route::get('admin/categorias/modificar/{id}', ['as' => 'admin/categorias/modificar', 'uses' => 'CategoriasController@mostrarCategoria'])->middleware('admin');
+Route::get('admin/pedidos/modificar/{id}', ['as' => 'admin/pedidos/modificar', 'uses' => 'PedidosController@mostrarPedido'])->middleware('admin');
+Route::get('admin/linpedidos/modificar/{num}/{pedido_id}', ['as' => 'admin/linpedidos/modificar', 'uses' => 'LinPedidosController@mostrarLinpedidosPedido'])->middleware('admin');
 
 // Eliminado de objetos de la bd
-Route::get('admin/productos/eliminar/{id}','ProductosController@delete');
-Route::get('admin/usuarios/eliminar/{id}', 'UsuariosController@delete');
-Route::get('admin/categorias/eliminar/{id}', 'CategoriasController@delete');
-Route::get('admin/pedidos/eliminar/{id}', 'PedidosController@delete');
-Route::get('admin/linpedidos/eliminar/{num}/{pedido_id}', 'LinPedidosController@delete');
+Route::get('admin/productos/eliminar/{id}','ProductosController@delete')->middleware('admin');
+Route::get('admin/usuarios/eliminar/{id}', 'UsuariosController@delete')->middleware('admin');
+Route::get('admin/categorias/eliminar/{id}', 'CategoriasController@delete')->middleware('admin');
+Route::get('admin/pedidos/eliminar/{id}', 'PedidosController@delete')->middleware('admin');
+Route::get('admin/linpedidos/eliminar/{num}/{pedido_id}', 'LinPedidosController@delete')->middleware('admin');
 
 // Añadido de objetos a la bd
-Route::post('admin/productos/anadir', 'ProductosController@create');
-Route::post('admin/usuarios/anadir', 'UsuariosController@create');
-Route::post('admin/categorias/anadir', 'CategoriasController@create');
-Route::post('admin/pedidos/anadir', 'PedidosController@create');
-Route::post('admin/linpedidos/anadir', 'LinPedidosController@create');
+Route::post('admin/productos/anadir', 'ProductosController@create')->middleware('admin');
+Route::post('admin/usuarios/anadir', 'UsuariosController@create')->middleware('admin');
+Route::post('admin/categorias/anadir', 'CategoriasController@create')->middleware('admin');
+Route::post('admin/pedidos/anadir', 'PedidosController@create')->middleware('admin');
+Route::post('admin/linpedidos/anadir', 'LinPedidosController@create')->middleware('admin');
 // Modificado de objetos de la bd
-Route::post('admin/productos/modificar/{id}', 'ProductosController@update');
-Route::post('admin/usuarios/modificar/{id}', 'UsuariosController@update');
-Route::post('admin/categorias/modificar/{id}', 'CategoriasController@update');
-Route::post('admin/pedidos/modificar/{id}', 'PedidosController@update');
-Route::post('admin/linpedidos/modificar/{num}/{pedido_id}', ['as' => 'admin/linpedidos/modificar', 'uses' => 'LinPedidosController@update']);
+Route::post('admin/productos/modificar/{id}', 'ProductosController@update')->middleware('admin');
+Route::post('admin/usuarios/modificar/{id}', 'UsuariosController@update')->middleware('admin');
+Route::post('admin/categorias/modificar/{id}', 'CategoriasController@update')->middleware('admin');
+Route::post('admin/pedidos/modificar/{id}', 'PedidosController@update')->middleware('admin');
+Route::post('admin/linpedidos/modificar/{num}/{pedido_id}', ['as' => 'admin/linpedidos/modificar', 'uses' => 'LinPedidosController@update'])->middleware('admin');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
